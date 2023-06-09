c462e1ee40abfa618afa625039c4540a57d16058
MartinRoberti23 <tinchoredfield@gmail.com>
2023-06-08 21:46:10 -0300

    tercercommit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express ();
 
 app.use(express.static("public"));
-
-app.listen (3000, ()=> console.log ('Servidor corriendo'));
+const port = process.env.PORT || 3000 ;
+app.listen (port, ()=> console.log ('Servidor corriendo' + port));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
