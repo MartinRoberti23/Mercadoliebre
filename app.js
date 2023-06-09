bfe95310462fb669277b1f99e5509aa7d3c312d8
MartinRoberti23 <tinchoredfield@gmail.com>
2023-06-08 22:55:26 -0300

    sithx

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require ('express')
 const path = require('path');
-const app = express ();
+const app = express();
 
 app.use(express.static("public"));
-const port = 3000 ;
-app.listen (port, ()=> console.log ('Servidor corriendo' + port));
+
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"))
+    res.sendFile(path.join(__dirname, "views" , "home.html"))
 });
+const port = 3000 ;
+
+app.listen(port, ()=> console.log ('Servidor corriendo' + port));
