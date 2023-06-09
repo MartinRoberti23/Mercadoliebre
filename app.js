f81c3b63b0bac4dcc37f022a78643ab87855dc88
MartinRoberti23 <tinchoredfield@gmail.com>
2023-06-08 22:07:22 -0300

    fifthcommit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express ();
 
 app.use(express.static("public"));
-const port = process.env.PORT || 3000 ;
+const port = 3000 ;
 app.listen (port, ()=> console.log ('Servidor corriendo' + port));
 
 app.get('/', (req, res) => {
